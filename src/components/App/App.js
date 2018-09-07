5ea3e17a7f7b00d28eb636b6b529dd86175066f5
Daniel MacKay <mackay2588@gmail.com>
2018-09-07 15:59:54 -0500

    redux logger and router installed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br />
+        </div>
+      </Router>
     );
   }
 }
